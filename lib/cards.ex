d4c6cbfba095c43445fcef9be81eb0b99a90da66
Davi Busanello <itsme@davibusanello.net>
2018-09-11 11:22:39 -0300

    Learning Elixir: comprehensions over lists

---
+++ lib/cards.ex
@@ -0,0 +1,35 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
+  end
+
+  @spec shuffle(any()) :: [any()]
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @spec contains?(any(), any()) :: boolean()
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
